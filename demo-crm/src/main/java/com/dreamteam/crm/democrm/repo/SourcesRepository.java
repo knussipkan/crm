ebc745db8b8333615b80a4bfad812a6f77354f73
Ondakanov <abzalkhan.ondakanov.000322550189@gmail.com>
2022-06-24 16:08:28 +0600

    Repositories have been added

---
+++ demo-crm/src/main/java/com/dreamteam/crm/democrm/repo/SourcesRepository.java
@@ -0,0 +1,34 @@
+package com.dreamteam.crm.democrm.repo;
+
+
+// реализует все необходимые запросы в БД
+
+import com.dreamteam.crm.democrm.entity.Sources;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.print.Doc;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+/*
+
+При запуске проекта Spring  находит все переменные, которые имеют тип этого интерфейса.
+
+После этого внедряет в эти переменные нужные объекты, где уже реализованы все методы.
+
+ */
+@Repository
+public interface SourcesRepository extends JpaRepository<Sources, Long> {
+   List<Sources> findSourcesByTitile(@Param("title") String title);
+
+
+}
